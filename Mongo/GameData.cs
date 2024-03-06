85312a4559f09691a5531bb9cd11a0612e563f06
Milan.Bella <Milan.Bella@aomvsr.sk>
2024-03-06 17:49:02 +0100

    m

--- Mongo/GameData.cs
+++ Mongo/GameData.cs
@@ -198,7 +198,7 @@ namespace Gaos.Mongo
                     {
                         Log.Information($"diff base: {gameDataJsonDiffBase}");
                         JObject gameDataJsonDiffBaseJObject = JObject.Parse(gameDataJsonDiffBase);
-                        Log.Information($"doc: {gameDataJsonDiffBase}");
+                        Log.Information($"doc: {doc.ToJson()}");
                         JObject gameDataJsontMongoJObject  = JObject.Parse(doc.ToJson());
                         var isBasesEqual = jsondiff.Difference.IsEqualValues(gameDataJsonDiffBaseJObject, gameDataJsontMongoJObject);
                         if (!isBasesEqual.IsEqual) {
