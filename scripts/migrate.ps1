8ea1ea5d013e310b97992ff112eefb6bc2e0f7b3
Milan Bella <milanbella>
2024-08-29 19:23:31 +0200

    m

--- scripts/migrate.ps1
+++ scripts/migrate.ps1
@@ -2,5 +2,8 @@ cd ..
 #dotnet ef database drop
 mysql -uroot -proot -e 'drop database gaos;create database gaos;'
 rm -r -force Migrations
+if (Test-Path -Path Migrations) {
+	Remove-Item -Path Migrations -Recurse -Force
+} 
 dotnet ef migrations add InitialCreate
 dotnet ef database update
